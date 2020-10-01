34010ed06f9a87cfb76ab65fc973dca78348680e
Johannes Kanefendt <johannes.kanefendt@krzn.de>
2020-10-01 10:38:22 +0200

    Authorisierung für die Raumerstellung gefixt. Erstellen von Jitis-Konferenzen an das Entitlement matrix-start-conference gebunden.

--- synapse/logineo/__init__.py
+++ synapse/logineo/__init__.py
@@ -13,41 +13,40 @@ class LogineoRules:
 		self.config = config
 		self.http_client = http_client
 
-	@defer.inlineCallbacks
 	def get_privileges(self, user_id):
-		privileges = yield self.http_client.get_json(self.config["endpoint_url"], headers={"Authenticated-User": [user_id]})
-		defer.returnValue(privileges)
+		return self.http_client.get_json(self.config["endpoint_url"], headers={"Authenticated-User": [user_id]})
 
-	@defer.inlineCallbacks
-	def check_event_allowed(self, event, state_events):
+	async def check_event_allowed(self, event, state_events):
 		allowed = True
+		content = event["content"]
 		if event.type == "m.room.member":
-			content = event["content"]
 			if content["membership"] == "invite" and not ("is_direct" in content and content["is_direct"]):
-				privileges = yield self.get_privileges(event.sender)
+				privileges = await self.get_privileges(event.sender)
 				allowed = "create-rooms" in privileges
+		elif event.type == "im.vector.modular.widgets":
+			if content["type"] == "jitsi":
+				privileges = await self.get_privileges(event.sender)
+				allowed = "start-conference" in privileges
 
-		defer.returnValue(allowed)
+		return allowed
 
-	@defer.inlineCallbacks
-	def on_create_room(self, requester, config, is_requester_admin):
+	async def on_create_room(self, requester, config, is_requester_admin):
 		allowed = False
 		user_id = requester.user.to_string()
-		privileges = yield self.get_privileges(user_id)
+		privileges = await self.get_privileges(user_id)
 
 		if "create-rooms" in privileges:
 			allowed = True
 		elif "is_direct" in config and config["is_direct"] and "create-direct-rooms" in privileges:
 			invitee = config["invite"][0]
 			checkUrl = self.config["endpoint_url"] + "/create-direct-rooms/check"
-			check = yield self.http_client.get_json(checkUrl, args={"subject": invitee}, headers={"Authenticated-User": [user_id]})
+			check = await self.http_client.get_json(checkUrl, args={"subject": invitee}, headers={"Authenticated-User": [user_id]})
 			allowed = check["allowed"]
 
-		defer.returnValue(allowed)
+		return allowed
 
-	@defer.inlineCallbacks
 	def check_threepid_can_be_invited(self, medium, address, state_events):
-		defer.returnValue(True)
+		return True
 
 	def parse_config(config):
 		endpoint_url = config.get("endpoint_url")
